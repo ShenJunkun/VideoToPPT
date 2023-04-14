dd313827fd23a1b67bd1e9e730d51f3d92191b4e
ShenJunkun <2458684728@qq.com>
2023-04-14 23:41:40 +0800

    finish ui

--- main.py
+++ main.py
@@ -6,20 +6,33 @@ from tkinter import *
 from pytube import YouTube
 import os
 from pptx.util import Inches
-
+import cv2
 from pptx import Presentation
 import threading
 import re
+import numpy as np
 
 def thread_it(func, *args):
     t = threading.Thread(target=func, args=args)
     t.setDaemon(True)
     t.start()
 
+def onProgress(stream, chunk, remains):
+    total = stream.filesize # 取得完整尺寸
+    percent = (total-remains)/total*100 # 減去剩餘尺寸(剩餘尺寸會抓取存取的檔案大小)
+    print(f'下载中 {percent:05.2f} %', end='\r') # 顯示進度，\r 表示不換行，在同一行更新
+
+def MSE(img1, img2):
+        squared_diff = (img1-img2)**2
+        summed = np.sum(squared_diff)
+        num_pix = img1.shape[0]*img1.shape[1]
+        err = summed/num_pix
+        return err
 
 
 def select_file():
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("all files", "*.*")))
+    filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File",filetypes = (("Video files", "*.mp4;*.mkv;*.avi;*.wmv;*.flv"), ("all files", "*.*"))
+)
     # 在entry中显示文件路径
     file_path_entry.delete(0, END)
     file_path_entry.insert(0, filename)
@@ -38,11 +51,24 @@ def video_to_ppt(filePath=None):
         os.mkdir('output')
     if filePath is None:
         filePath = 'input.mp4'
-    os.system('ffmpeg -loglevel error -i {filePath} -vf fps=1 output/out%d.png')
+    os.system(f'ffmpeg -loglevel error -i {filePath} -vf fps=1 output/out%d.png')
     stext.insert(tk.END, "图片生成完成\n\n")
 
     stext.insert(tk.END, "开始处理图片...\n")
     #TODO 处理图片，去掉部分相似的图片
+
+    fileNum = len([x for x in os.listdir('output/')]) # read images
+    imgPrev = cv2.imread('output/out1.png') # first image
+    for i in range(fileNum-1):
+        filePath = 'output/out'+ str(i+2) +'.png'
+        img = cv2.imread(filePath)
+        grayA = cv2.cvtColor(imgPrev, cv2.COLOR_BGR2GRAY)
+        grayB = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # check threshold and delete images
+        if MSE(grayA,grayB) < 4:
+            os.remove(filePath)
+        #print(str(i) + ' MSE: ' + str(MSE(grayA,grayB)))
+        imgPrev = img
 
     stext.insert(tk.END, "图片处理完成\n\n")
 
@@ -64,7 +90,9 @@ def video_to_ppt(filePath=None):
 
 def end_to_end():
     download_video()
+    print("end to end ...")
     video_to_ppt()
+
 
 
 if __name__ == "__main__":
@@ -110,7 +138,7 @@ if __name__ == "__main__":
     select_button = Button(frame_local_generate, text="选择文件", font=('楷体', 12), command=select_file)
     select_button.pack(side=tk.LEFT, padx=spacing, pady=10)
 
-    button3 = tk.Button(frame_local_generate, text="本地生成", font=('楷体', 12))
+    button3 = tk.Button(frame_local_generate, text="本地生成", font=('楷体', 12),  command=lambda :thread_it(video_to_ppt, file_path_entry.get()))
     button3.pack(side=tk.LEFT, padx=spacing, pady=10)
 
     stext = ScrolledText(width=60, height=18, background='#F7F3EC')
